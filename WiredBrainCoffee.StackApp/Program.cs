14f1d38bd7458eb88fd0375e1a5f8a58364aa43c
Hyunkyoung Lee <HLee@1800contacts.com>
2023-02-22 13:51:37 -0700

    C#9 Generics-2-4

--- WiredBrainCoffee.StackApp/Program.cs
+++ WiredBrainCoffee.StackApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace WiredBrainCoffee.StackApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var stack = new SimpleStack();
+        stack.Push(1.2);
+        stack.Push(3.5);
+        stack.Push(4.8);
+        stack.Push(1.9);
+
+    }
+}
---
+++ WiredBrainCoffee.StackApp/SimpleStack.cs
@@ -0,0 +1,17 @@
+﻿namespace WiredBrainCoffee.StackApp
+{
+    internal class SimpleStack
+    {
+        private readonly double[] _items;
+
+        public SimpleStack()
+        {
+            _items = new double[10];
+        }
+
+        public void Push(double item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
